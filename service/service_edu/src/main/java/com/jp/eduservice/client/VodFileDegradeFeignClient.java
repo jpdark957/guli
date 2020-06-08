9fb1ebcc93425481c0c631b8d8790ca1508b6765
jpdark <1072500236@qq.com>
2020-06-08 08:42:43 +0800

    ''

--- service/service_edu/src/main/java/com/jp/eduservice/client/VodFileDegradeFeignClient.java
+++ service/service_edu/src/main/java/com/jp/eduservice/client/VodFileDegradeFeignClient.java
@@ -1,12 +1,14 @@
 package com.jp.eduservice.client;
 
 import com.jp.commonutils.R;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 /**
  * 删除多个视频出错了
  */
+@Component
 public class VodFileDegradeFeignClient implements VodClient{
     @Override
     public R removeAlyVideo(String id) {
